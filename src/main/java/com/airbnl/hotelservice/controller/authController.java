2afce719216d5bc09bfd60adce713cc3c364074e
omar <omar10zoubi@gmail.com>
2023-03-11 02:22:03 +0300

    feat: added login logic

---
+++ src/main/java/com/airbnl/hotelservice/controller/authController.java
@@ -0,0 +1,45 @@
+package com.airbnl.hotelservice.controller;
+
+import com.airbnl.hotelservice.model.User;
+import com.airbnl.hotelservice.service.interfaces.IUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.RedirectView;
+
+@Controller
+@RequestMapping("/user")
+public class authController {
+    private final IUserService userService;
+    public authController(IUserService userService){
+        this.userService = userService;
+    }
+    @GetMapping(path = "/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping(path = "/signup")
+    public String signup(Model model) {
+        return "signup";
+    }
+    @PostMapping(path = "/login")
+    public RedirectView  login(User user) {
+        User managerFromDb = userService.getByUserName(user.getUsername());
+        if(managerFromDb == null)
+            return new RedirectView("/hotels/AllByManagerID/" + managerFromDb.getId());
+
+        return new RedirectView("/user/login");
+    }
+
+    @PostMapping(path = "/signup")
+    public RedirectView  signup(User user) {
+        userService.save(user);
+        int managerId = userService.getByUserName(user.getUsername()).getId();
+
+        return new RedirectView("/hotels/AllByManagerID/" + managerId);
+    }
+
+}
